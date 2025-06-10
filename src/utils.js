98d94cf2ea5994e379397d627a8ee599e5d647af
Kavy Jaiswal <kavyjaiswal4@gmail.com>
2025-06-10 09:54:07 +0530

    updated base URL

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://sms-backend-2qeh.onrender.com/api",
+    baseURL: "https://dairy-backend-m9m2.onrender.com/api",
     withCredentials: true,
 });
 
